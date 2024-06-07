b31dab7ec1790ca8ec1ac21797f939a0585e0f1d
Dev_Rungta <devrungta22@gmail.com>
2024-06-07 13:05:05 +0530

    feat: Added attendance layout to home screen

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:csoc_flutter/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'attendance.dart';
+
 
 // Can also access current User by AuthService().currentUser globally in the app
 
@@ -22,48 +22,82 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     AuthCubit authCubit = context.read<AuthCubit>();
     return Scaffold(
-      appBar:   const CustomAppBar(
+      appBar: const CustomAppBar(
         title: "CSOC Flutter",
         backgroundColor: AppColors.primaryColor,
         actions: [
-
         ],
 
       ),
       body: Column(
         children: [
           Text(widget.user.name!),
-
-
-
-
           ElevatedButton(
             onPressed: authCubit.signOut,
             child: const Text("Sign-out"),
           ),
-        ],
-      ),
-      bottomNavigationBar:
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Column(
-            children: [
-              IconButton(onPressed: ()=> {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const Attendance()))
-              }, icon: const Icon(Icons.menu_book_rounded) ),
-              const Text('Attendance')
-            ],
-          ),
-          Column(
-            children: [
-              IconButton(onPressed:()=>{} //Todo: Add the reminder page.
-                  , icon: const Icon(Icons.book))
-            ],
-          )
-        ],
+      Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text('Attendance', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 20),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Icon(Icons.circle, color: Colors.green),
+                    Text('Present'),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.circle, color: Colors.red),
+                    Text('Absent'),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.circle, color: Colors.blue),
+                    Text('Proxied'),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.circle, color: Colors.yellow),
+                    Text('Cancelled'),
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(height: 20),
+            const SizedBox(
+                height: 200,
+                child: SingleChildScrollView(
+                    child: Column(
+                        children: [
+                          //TODO: Add a classes class and display the classes on the selected date here.
+                        ]
+                    )
+                )
+            ),
+            const SizedBox(height: 20),
 
+            ElevatedButton(
+              onPressed: () {
+                //TODO: Add logic to add extra class
+              },
+              child: const Text('Add Extra Class'), ),
+            ElevatedButton(onPressed: (){
+              Navigator.of(context).pop();
+            }, child: const Text('Back'))
+          ],
+        ),
       ),
-    );
+      ],)
+
+      );
+
   }
 }
