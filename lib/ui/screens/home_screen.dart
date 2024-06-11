4f98269c19d6d27028e600997d42f19c2fa1cf50
Dev_Rungta <devrungta22@gmail.com>
2024-06-11 11:06:14 +0530

    feat: Added the attendance layout to home screen

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:csoc_flutter/cubit/auth_cubit.dart';
+import 'package:csoc_flutter/models/subjects.dart';
 import 'package:csoc_flutter/models/user_model.dart';
 import 'package:csoc_flutter/ui/widgets/app_bar.dart';
 import 'package:csoc_flutter/utils/colors.dart';
@@ -8,7 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../models/attendance.dart';
 // Can also access current User by AuthService().currentUser globally in the app
 
 class HomeScreen extends StatefulWidget {
@@ -46,153 +46,155 @@ class _HomeScreenState extends State<HomeScreen> {
           backgroundColor: AppColors.primaryColor,
           actions: [],
         ),
-        body: Column(
-          children: [
-            Text(widget.user.name!),
-            ElevatedButton(
-              onPressed: authCubit.signOut,
-              child: const Text("Sign-out"),
-            ),
-            Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Text('Attendance',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                  const SizedBox(height: 20),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children: [
-                          Icon(Icons.circle, color: Colors.green),
-                          Text('Present'),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Icon(Icons.circle, color: Colors.red),
-                          Text('Absent'),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Icon(Icons.circle, color: Colors.yellow),
-                          Text('Cancelled'),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Icon(Icons.circle, color: Colors.blue),
-                          Text('Proxied'),
-                        ],
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    height: 400, // Constrains the ListView's height
-                    child: FutureBuilder<List<Subject>>(
-                      future: subjects,
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return const Center(
-                              child: CircularProgressIndicator());
-                        } else if (snapshot.hasError) {
-                          return Center(
-                              child: Text('Error: ${snapshot.error}'));
-                        } else {
-                          final subjects = snapshot.data!;
-                          return Material(
-                            child: ListView.builder(
-                              padding: const EdgeInsets.all(8.0),
-                              itemCount: subjects.length,
-                              itemBuilder: (context, index) {
-                                final subject = subjects[index];
-                                return Container(
-                                  margin:
-                                      const EdgeInsets.symmetric(vertical: 8.0),
-                                  decoration: BoxDecoration(
-                                    color: Colors.cyanAccent,
-                                    borderRadius: BorderRadius.circular(15.0),
-                                  ),
-                                  child: ListTile(
-                                    title: Center(
-                                        child: Text(subject.subjectName)),
-                                    subtitle: Padding(
-                                      padding: const EdgeInsets.all(5.0),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                              "Real Attendance: ${subject.realAttendance}"),
-                                          Text(
-                                              "Proxied Attendance: ${subject.attendanceWithProxies}"),
-                                          Text(
-                                              "Target Attendance: ${subject.targetAttendance}"),
-                                          const Text(
-                                              "Attend/Can Bunk ------- Classes"),
-                                          Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceAround,
-                                            children: <Widget>[
-                                              IconButton(
-                                                onPressed: () {},
-                                                icon: const Icon(Icons.circle,
-                                                    color: Colors.green),
-                                              ),
-                                              IconButton(
-                                                onPressed: () {},
-                                                icon: const Icon(Icons.circle,
-                                                    color: Colors.red),
-                                              ),
-                                              IconButton(
-                                                onPressed: () {},
-                                                icon: const Icon(Icons.circle,
-                                                    color: Colors.blue),
-                                              ),
-                                              IconButton(
-                                                onPressed: () {},
-                                                icon: const Icon(Icons.circle,
-                                                    color: Colors.yellow),
-                                              ), //TODO: Add logic in these buttons
-                                            ],
-                                          ),
-                                        ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text(widget.user.name!),
+              ElevatedButton(
+                onPressed: authCubit.signOut,
+                child: const Text("Sign-out"),
+              ),
+              Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text('Attendance',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 20),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            Icon(Icons.circle, color: Colors.green),
+                            Text('Present'),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.circle, color: Colors.red),
+                            Text('Absent'),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.circle, color: Colors.yellow),
+                            Text('Cancelled'),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.circle, color: Colors.blue),
+                            Text('Proxied'),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      height: 400, // Constrains the ListView's height
+                      child: FutureBuilder<List<Subject>>(
+                        future: subjects,
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const Center(
+                                child: CircularProgressIndicator());
+                          } else if (snapshot.hasError) {
+                            return Center(
+                                child: Text('Error: ${snapshot.error}'));
+                          } else {
+                            final subjects = snapshot.data!;
+                            return Material(
+                              child: ListView.builder(
+                                padding: const EdgeInsets.all(8.0),
+                                itemCount: subjects.length,
+                                itemBuilder: (context, index) {
+                                  final subject = subjects[index];
+                                  return Container(
+                                    margin: const EdgeInsets.symmetric(
+                                        vertical: 8.0),
+                                    decoration: BoxDecoration(
+                                      color: Colors.cyanAccent,
+                                      borderRadius: BorderRadius.circular(15.0),
+                                    ),
+                                    child: ListTile(
+                                      title: Center(
+                                          child: Text(subject.subjectName)),
+                                      subtitle: Padding(
+                                        padding: const EdgeInsets.all(5.0),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                                "Real Attendance: ${subject.realAttendance}"),
+                                            Text(
+                                                "Proxied Attendance: ${subject.attendanceWithProxies}"),
+                                            Text(
+                                                "Target Attendance: ${subject.targetAttendance}"),
+                                            const Text(
+                                                "Attend/Can Bunk ------- Classes"),
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.spaceAround,
+                                              children: <Widget>[
+                                                IconButton(
+                                                  onPressed: () {},
+                                                  icon: const Icon(Icons.circle,
+                                                      color: Colors.green),
+                                                ),
+                                                IconButton(
+                                                  onPressed: () {},
+                                                  icon: const Icon(Icons.circle,
+                                                      color: Colors.red),
+                                                ),
+                                                IconButton(
+                                                  onPressed: () {},
+                                                  icon: const Icon(Icons.circle,
+                                                      color: Colors.blue),
+                                                ),
+                                                IconButton(
+                                                  onPressed: () {},
+                                                  icon: const Icon(Icons.circle,
+                                                      color: Colors.yellow),
+                                                ), //TODO: Add logic in these buttons
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(15),
                                       ),
                                     ),
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(15),
-                                    ),
-                                  ),
-                                );
-                              },
-                            ),
-                          );
-                        }
-                      },
+                                  );
+                                },
+                              ),
+                            );
+                          }
+                        },
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      // TODO: Add logic to add extra class
-                    },
-                    child: const Text('Add Extra Class'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text('Back'),
-                  ),
-                ],
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        // TODO: Add logic to add extra class
+                      },
+                      child: const Text('Add Extra Class'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Back'),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ));
   }
 }
